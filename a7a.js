50c81e9f4bd3b4308f2f45ea8577f18dbb45a692
SaifOmar <aliensaif722@gmail.com>
2025-04-23 09:47:19 +0200

    refactor: good refactor good

--- a7a.js
+++ a7a.js
@@ -1,5 +1,10 @@
-// import { Game } from "./entities/entities.js";
-// export const game = new Game();
-// let n = 6;
-// game.Start();
-// game.Debug();
+import { Game } from "./entities/game.js";
+
+export const game = new Game();
+
+let n = 6;
+const ps = ["hello", "ss", "fkldasjfkl", "sdghlasdgf", "dfhasld", "sassif"];
+game.SetPlayerNames(ps);
+game.Start();
+
+game.Debug();
